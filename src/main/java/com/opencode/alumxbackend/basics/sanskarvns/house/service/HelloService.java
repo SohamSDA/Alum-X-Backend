086d073501117d023b2bcbf26ce9a9727e742d4c
Aryan9059 <99357111+Aryan9059@users.noreply.github.com>
2026-01-21 00:47:51 +0530

    Cleaned up code

--- src/main/java/com/opencode/alumxbackend/basics/sanskarvns/house/service/HelloService.java
+++
@@ -1,7 +0,0 @@
-package com.opencode.alumxbackend.basics.sanskarvns.house.service;
-
-public class HelloService {
-    public String getHelloMessage() {
-        return "Hello from HelloService!";
-    }
-}
